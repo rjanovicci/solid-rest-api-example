e287e658e60ebdf4dab4f9b871b50299babdf815
rjanovicci <rjanovicci@hotmail.com>
2022-03-27 14:21:44 -0300

    feat: add CreateUserController

---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+  constructor(
+    private createUserUseCase: CreateUserUseCase,
+  ) {}
+  
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+    console.log('chegou aqui')
+
+    try {
+      await this.createUserUseCase.execute({
+        name,
+        email,
+        password,
+      })
+  
+      return res.status(201).json({ message: 'Created user' });
+    } catch (err) {
+      return res.status(400).json({
+        message: err.message || 'Unexpected error.',
+      })
+    }
+  }
+}
